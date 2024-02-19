1d60a724f89c470c83f84ff794b3c48a55734785
Denys <geroyqw@gmail.com>
2024-02-19 17:59:10 +0200

    Оновлено файл з реалізованим контролером

--- text_editor.py
+++ text_editor.py
@@ -1,121 +1,96 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QVBoxLayout, QWidget, QPushButton, QMessageBox, QFileDialog, QFontDialog, QColorDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QPushButton, QFileDialog, QFontDialog, QColorDialog
 
 
-class TextEditor(QMainWindow):
+class TextEditorModel:
+    def __init__(self):
+        self.text = ""
+
+    def load_text(self, filename):
+        with open(filename, "r") as file:
+            self.text = file.read()
+
+    def save_text(self, filename, text):
+        with open(filename, "w") as file:
+            file.write(text)
+
+
+class TextEditorController:
+    def __init__(self, view, model):
+        self.view = view
+        self.model = model
+
+        self.view.new_button.clicked.connect(self.new_file)
+        self.view.open_button.clicked.connect(self.open_file)
+        self.view.save_button.clicked.connect(self.save_file)
+        self.view.font_button.clicked.connect(self.change_font)
+        self.view.text_color_button.clicked.connect(self.change_text_color)
+        self.view.bg_color_button.clicked.connect(self.change_bg_color)
+
+    def new_file(self):
+        self.view.text_edit.clear()
+
+    def open_file(self):
+        filename, _ = QFileDialog.getOpenFileName(self.view, "Відкрити файл", "", "Text files (*.txt);;All files (*.*)")
+        if filename:
+            self.model.load_text(filename)
+            self.view.text_edit.setText(self.model.text)
+
+    def save_file(self):
+        filename, _ = QFileDialog.getSaveFileName(self.view, "Зберегти файл", "", "Text files (*.txt);;All files (*.*)")
+        if filename:
+            text = self.view.text_edit.toPlainText()
+            self.model.save_text(filename, text)
+
+    def change_font(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            self.view.text_edit.setFont(font)
+
+    def change_text_color(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.view.text_edit.setTextColor(color)
+
+    def change_bg_color(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.view.text_edit.setStyleSheet("background-color: {}".format(color.name()))
+
+
+class TextEditorView(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
-
-    def initUI(self):
         self.setWindowTitle("Текстовий редактор")
         self.setGeometry(100, 100, 800, 600)
 
-        self.textEdit = QTextEdit()
-        self.setCentralWidget(self.textEdit)
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setGeometry(10, 10, 780, 480)
 
-        self.createActions()
-        self.createMenus()
-        self.createToolBar()
+        self.new_button = QPushButton("Новий", self)
+        self.new_button.setGeometry(10, 500, 80, 30)
 
-    def createActions(self):
-        self.newAction = QAction("&Новий", self)
-        self.newAction.setShortcut("Ctrl+N")
-        self.newAction.triggered.connect(self.newFile)
+        self.open_button = QPushButton("Відкрити", self)
+        self.open_button.setGeometry(100, 500, 80, 30)
 
-        self.openAction = QAction("&Відкрити", self)
-        self.openAction.setShortcut("Ctrl+O")
-        self.openAction.triggered.connect(self.openFile)
+        self.save_button = QPushButton("Зберегти", self)
+        self.save_button.setGeometry(190, 500, 80, 30)
 
-        self.saveAction = QAction("&Зберегти", self)
-        self.saveAction.setShortcut("Ctrl+S")
-        self.saveAction.triggered.connect(self.saveFile)
+        self.font_button = QPushButton("Шрифт", self)
+        self.font_button.setGeometry(280, 500, 80, 30)
 
-        self.exitAction = QAction("&Вихід", self)
-        self.exitAction.setShortcut("Ctrl+Q")
-        self.exitAction.triggered.connect(self.close)
+        self.text_color_button = QPushButton("Колір тексту", self)
+        self.text_color_button.setGeometry(370, 500, 110, 30)
 
-        self.fontAction = QAction("&Змінити шрифт", self)
-        self.fontAction.setShortcut("Ctrl+F")
-        self.fontAction.triggered.connect(self.changeFont)
-
-        self.textColorAction = QAction("&Колір тексту", self)
-        self.textColorAction.triggered.connect(self.changeTextColor)
-
-        self.bgColorAction = QAction("&Колір фону", self)
-        self.bgColorAction.triggered.connect(self.changeBgColor)
-
-    def createMenus(self):
-        menubar = self.menuBar()
-
-        fileMenu = menubar.addMenu("&Файл")
-        fileMenu.addAction(self.newAction)
-        fileMenu.addAction(self.openAction)
-        fileMenu.addAction(self.saveAction)
-        fileMenu.addAction(self.exitAction)
-
-        formatMenu = menubar.addMenu("&Формат")
-        formatMenu.addAction(self.fontAction)
-        formatMenu.addAction(self.textColorAction)
-        formatMenu.addAction(self.bgColorAction)
-
-    def createToolBar(self):
-        toolbar = self.addToolBar("Засоби")
-
-        toolbar.addAction(self.newAction)
-        toolbar.addAction(self.openAction)
-        toolbar.addAction(self.saveAction)
-
-        fontButton = QPushButton("Шрифт", self)
-        fontButton.clicked.connect(self.changeFont)
-        toolbar.addWidget(fontButton)
-
-        textColorButton = QPushButton("Колір тексту", self)
-        textColorButton.clicked.connect(self.changeTextColor)
-        toolbar.addWidget(textColorButton)
-
-        bgColorButton = QPushButton("Колір фону", self)
-        bgColorButton.clicked.connect(self.changeBgColor)
-        toolbar.addWidget(bgColorButton)
-
-    def newFile(self):
-        self.textEdit.clear()
-
-    def openFile(self):
-        filename, _ = QFileDialog.getOpenFileName(self, "Відкрити файл", "", "Text files (*.txt);;All files (*.*)")
-
-        if filename:
-            with open(filename, "r") as file:
-                text = file.read()
-                self.textEdit.setText(text)
-
-    def saveFile(self):
-        filename, _ = QFileDialog.getSaveFileName(self, "Зберегти файл", "", "Text files (*.txt);;All files (*.*)")
-
-        if filename:
-            with open(filename, "w") as file:
-                text = self.textEdit.toPlainText()
-                file.write(text)
-
-    def changeFont(self):
-        font, ok = QFontDialog.getFont()
-        if ok:
-            self.textEdit.setFont(font)
-
-    def changeTextColor(self):
-        color = QColorDialog.getColor()
-        if color.isValid():
-            self.textEdit.setTextColor(color)
-
-    def changeBgColor(self):
-        color = QColorDialog.getColor()
-        if color.isValid():
-            self.textEdit.setStyleSheet("background-color: {}".format(color.name()))
+        self.bg_color_button = QPushButton("Колір фону", self)
+        self.bg_color_button.setGeometry(490, 500, 110, 30)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    editor = TextEditor()
-    editor.show()
+    view = TextEditorView()
+    model = TextEditorModel()
+    controller = TextEditorController(view, model)
+    view.show()
     sys.exit(app.exec_())
